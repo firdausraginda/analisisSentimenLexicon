11f094c9f28c7a77f26d84244b0e7bca3de5ea8a
firdausraginda <ragindafirdaus01@gmail.com>
2019-03-09 20:58:01 +0700

    last update lexicon custom

--- lexiconCustom.py
+++ lexiconCustom.py
@@ -1,1 +1,3 @@
-lexCustom = [['saya', 0], ['dia', 0], ['tidak ada', 0], ['ga ada', 0], ['yang', 0], ['pa', 0], ['bagus', 2]]
+lexCustom = [['saya', 0], ['tidak ada', 0], ['ga ada', 0], ['yang', 0], ['pa', 0], ['bagus', 2]]
+# bagus = baik
+# ga ada = tidak ada
