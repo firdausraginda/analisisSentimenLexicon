ed1034bef673e8af52ce6db8545d1d25590b73e2
firdausraginda <ragindafirdaus01@gmail.com>
2019-03-27 19:44:08 +0700

    update lexicon custom

--- lexiconCustom.py
+++ lexiconCustom.py
@@ -1,8 +1,8 @@
-lexCustom = [['saya', 0], ['aku', 0], ['selama', 0], ['tidak ada', 0], ['ga ada', 0], ['yang', 0], ['pa', 0], ['bagus', 2], ['keren', 2],['sangat baik', 4], ['sangat bagus', 4], ['sangat menyenangkan', 3],['cukup', 1], ['masukkan', -1], ['masukan', -1]]
-# bagus = baik = 2
+lexCustom = [['saya', 0], ['aku', 0], ['selama', 0], ['tidak ada', 0], ['ga ada', 0], ['yang', 0], ['pa', 0], ['bagus', 2], ['keren', 2],['sangat baik', 4], ['sangat bagus', 4], ['sangat menyenangkan', 3], ['cukup', 1], ['masukkan', -1], ['masukan', -1]]
+# selama != lama/lelet/pelan, lebih menunjukkan durasi waktu
 # ga ada = tidak ada
-# sangat baik = +2 dan +2
-# sangat baik = sangat bagus
+# bagus = baik = keren = 2
+# sangat baik = sangat bagus = +2 dan +2
 # sangat menyenangkan = +2 dan +1
 # cukup = 0.5 * baik/bagus
 # masukkan dan masukan = saran = -1
