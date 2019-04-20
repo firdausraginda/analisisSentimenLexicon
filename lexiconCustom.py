5c7a9fb19f2cec227c9f68caa2b45d962a00220e
firdausraginda <ragindafirdaus01@gmail.com>
2019-04-20 15:56:32 +0700

    setting lexicon default to inset lexicon modified

--- lexiconCustom.py
+++ lexiconCustom.py
@@ -1,8 +1,8 @@
 # tanpa custom lex, akurasi lvl dokumen 90% tapi lvl kalimat rendah
-lexCustom = []
+# lexCustom = []
 
 # akurasi lvl dokumen 90% dan lvl kalimat lebih tinggi
-# lexCustom = [['tidak ada', 0], ['ga ada', 0], ['masukkan', -1], ['masukan', -1]]
+lexCustom = [['tidak ada', 0], ['ga ada', 0], ['masukkan', -1], ['masukan', -1]]
 
 # ga ada = tidak ada = 0
 # masukkan dan masukan = saran = -1
